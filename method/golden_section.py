19e069fafb29f132a3955808b3ba25135a253842
Ng Kai Zhe <kaizhe1991@hotmail.com>
2019-05-20 15:26:10 +0800

    include Equation into powell(still not yet finished, as some bugs detected)

--- method/golden_section.py
+++ method/golden_section.py
@@ -1,15 +1,17 @@
-from method.TestEquation import TestingEquation as Equation
 from math import sqrt
+from Equation import Equation
+from typing import List
 
 
 error_value = 0.0000001
 GR = float(sqrt(5) - 1) / 2
 
 
-def golden_section(lower_bound: float, upper_bound: float, equation: Equation, p: list, vector: list) -> float:
+def golden_section(equation: Equation, vars_form: List[str], lower_bound: float, upper_bound: float, p: list, vector: list) -> float:
     a = lower_bound
     b = upper_bound
-    func = equation.compute
+    func = equation.eval_normal_form
+    total_var = len(vars_form)
     d = GR * (b - a)
     x1 = a + d
     x2 = b - d
@@ -17,13 +19,17 @@ def golden_section(lower_bound: float, upper_bound: float, equation: Equation, p
 
     while abs(x1 - x2) > error_value and N < 100000:
         parameter_list = []
-        for i in range(equation.total_var):
+        for i in range(total_var):
             parameter_list.append(p[i] + x1 * vector[i])
-        f1 = func(parameter_list)
+
+        vars_dict = build_var_dict(vars_form, parameter_list)
+        f1 = func(vars_dict)
         parameter_list = []
-        for i in range(equation.total_var):
+        for i in range(total_var):
             parameter_list.append(p[i] + x2 * vector[i])
-        f2 = func(parameter_list)
+
+        vars_dict = build_var_dict(vars_form, parameter_list)
+        f2 = func(vars_dict)
 
         if f1 < f2:
             a = x2
@@ -37,6 +43,14 @@ def golden_section(lower_bound: float, upper_bound: float, equation: Equation, p
         N += 1
 
     return x1
+
+
+def build_var_dict(vars_form: List[str], vars_value: List[float]):
+    vars_dict = {}
+    for i in range(len(vars_value)):
+        vars_dict[vars_form[i]] = vars_value[i]
+
+    return vars_dict
 
 
 if __name__ == '__main__':
--- method/powell.py
+++ method/powell.py
@@ -1,8 +1,9 @@
 from typing import List
 from math import sqrt
-from method.TestEquation import TestingEquation as Equation
-from method.golden_section import golden_section
+from method.golden_section import golden_section, build_var_dict
 import numpy as np
+from Equation import Equation
+from Exception.explosion import Explosion
 
 
 maximum_value = 99999999
@@ -10,22 +11,26 @@ minimum_value = -99999999
 error_value = 0.0000001
 
 
-def powell(equation_str: str, initial_point: List[float], interval: List[List[float]]):
+def powell(equation_str: str, vars_form: List[str], initial_point: List[float], interval: List[List[float]]):
     answer = ''
     equation = Equation(equation_str)
-    total_var = equation.total_var
+    total_var = len(vars_form)
 
     # check input type whether correct or not
-    assert len(initial_point) == total_var == len(interval)
+    if (len(initial_point) == total_var == len(interval)) is False:
+        Explosion.POWELL_LENGTH_INTERVAL_INITIAL_POINT_NOT_SAME_AS_INPUT_EQUATION.bang()
     for i in interval:
-        assert len(i) == 2
+        if len(i) == 2 is False:
+            Explosion.POWELL_LENGTH_INTERVAL_MUST_BE_ONLY_TWO.bang()
 
     # total iteration
     N = 100
     X = np.array([initial_point])
     S = np.identity(total_var)
 
-    answer += ('First loop: f(%s)= %s\n\n' % (X[0], equation.compute(X[0])))
+    # build dict
+    vars_dict = build_var_dict(vars_form, X[0])
+    answer += ('First loop: f(%s)= %s\n\n' % (X[0], equation.eval_normal_form(vars_dict)))
 
     for i in range(N):
         # check break situation:
@@ -40,10 +45,11 @@ def powell(equation_str: str, initial_point: List[float], interval: List[List[fl
 
         for j in range(total_var):
             alpha_lb, alpha_ub = get_lb_ub(interval, P[j], S[j])
-            alphas[j] = golden_section(alpha_lb, alpha_ub, equation, P[j], S[j])
+            alphas[j] = golden_section(equation, vars_form, alpha_lb, alpha_ub, P[j], S[j])
             P = np.append(P, [P[j] + alphas[j]*S[j]], axis=0)
 
-            answer += ('i=%s\nj=%s\nalpha=%s\nf(%s)=%s\n\n' % (i, j, alphas[j], P[j+1], equation.compute(P[j+1])))
+            vars_dict = build_var_dict(vars_form, P[j+1])
+            answer += ('i=%s\nj=%s\nalpha=%s\nf(%s)=%s\n\n' % (i, j, alphas[j], P[j+1], equation.eval_normal_form(vars_dict)))
 
         # The new displacement vector(summation alphas[i]*S[i] from 0 to total_var-1) becomes a new search vector
         sn = P[total_var] - P[0]
@@ -62,8 +68,10 @@ def powell(equation_str: str, initial_point: List[float], interval: List[List[fl
         answer += ('New S{%s}\n\n' % S)
 
     answer += ('X Set = {%s}\n\n' % X)
-    answer += ('Last loop: f(%s)= %s\n\n' % (X[len(X)-1], equation.compute(X[len(X)-1])))
-    return answer
+    # build dict
+    vars_dict = build_var_dict(vars_form, X[len(X)-1])
+    answer += ('Last loop: f(%s)= %s\n\n' % (X[len(X)-1], equation.eval_normal_form(vars_dict)))
+    return answer, X
 
 
 # get magnitude
@@ -101,9 +109,15 @@ def get_lb_ub(interval: List[List[float]], pi: List[float], si: List[float]):
 
 if __name__ == '__main__':
     # print('Q1: x^2+x-2*x^0.5')
-    # print(powell(equation_str='???', initial_point=[50], interval=[[0, 70]]))
+    # answer, X = powell(equation_str='x^2+x-2*x^0.5', vars_form=['x'], initial_point=[50], interval=[[0, 70]])
+    # print(answer)
     # print('Q2: sin(3x)+cos(x)')
-    # print(powell(equation_str='???', initial_point=[1], interval=[[0.3, 3]]))
-    print('Q2: 7 + x^2 - 3*x*y + 3.25*y^2 - 4*y')
-    print(powell(equation_str='???', initial_point=[50, 30], interval=[[-50, 70], [-70, 70]]))
+    # answer, X = powell(equation_str='sin(3*x)+cos(x)', vars_form=['x'], initial_point=[50], interval=[[0, 70]])
+    # print(answer)
+    # print('Q2: 7 + x^2 - 3*x*y + 3.25*y^2 - 4*y')
+    # answer, X = powell(equation_str='7 + x^2 - 3*x*y + 3.25*y^2 - 4*y', vars_form=['x'], initial_point=[50], interval=[[0, 70]])
+    # print(answer)
+    equation1 = Equation('x^2+x-2*x^0.5')
+    print(equation1.eval_normal_form({'x': 50}))
     pass
+
